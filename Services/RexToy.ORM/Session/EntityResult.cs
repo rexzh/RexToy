fefdee1efaed3c841d6ef45a0acf595a727e626c
rex <ling,zhang9@hp.com>
2016-04-28 17:16:37 +0800

    update API.

--- Services/RexToy.ORM/Session/EntityResult.cs
+++ Services/RexToy.ORM/Session/EntityResult.cs
@@ -8,30 +8,16 @@ namespace RexToy.ORM.Session
     public class EntityResult<T>
     {
         private IEntityManager _mgr;
-        public EntityResult(IEntityManager mgr)
+        internal EntityResult(IEntityManager mgr)
         {
             _mgr = mgr;
         }
-
-        private Expression<Func<T, object>> _orderby;
-        private OrderType _order;
-        public EntityResult<T> Orderby(Expression<Func<T, object>> orderby, OrderType order = OrderType.Asc)
+        
+        public OrderedEntityResult<T> Orderby(Expression<Func<T, object>> orderby)
         {
-            if (_orderby != null)
-            {
-                //TODO:throw
-            }
-            _orderby = orderby;
-            _order = order;
-            return this;
-        }
-
-        public List<T> Top(int top)
-        {
-            if (_where == null)
-                return _mgr.FindBy(_orderby, top, _order);
-            else
-                return _mgr.FindBy(_where, _orderby, top, _order);
+            orderby.ThrowIfNullArgument(nameof(orderby));
+            var r = new OrderedEntityResult<T>(_mgr, _where, orderby);
+            return r;
         }
 
         private Expression<Func<T, bool>> _where;
---
+++ Services/RexToy.ORM/Session/OrderedEntityResult.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace RexToy.ORM.Session
+{
+    public class OrderedEntityResult<T>
+    {
+        private IEntityManager _mgr;
+        private Expression<Func<T, object>> _orderby;
+        private Expression<Func<T, bool>> _where;
+        private OrderType _order;
+        internal OrderedEntityResult(IEntityManager mgr, Expression<Func<T, bool>> where, Expression<Func<T, object>> orderby)
+        {
+            _mgr = mgr;
+            _where = where;
+            _orderby = orderby;
+        }
+
+        public List<T> Top(int top)
+        {
+            if (_where == null)
+                return _mgr.FindBy(_orderby, top, _order);
+            else
+                return _mgr.FindBy(_where, _orderby, top, _order);
+        }
+
+        public OrderedEntityResult<T> Asc()
+        {
+            _order = OrderType.Asc;
+            return this;
+        }
+
+        public OrderedEntityResult<T> Desc()
+        {
+            _order = OrderType.Desc;
+            return this;
+        }
+    }
+}
